358c4a231f6b050c806f4db1dc11d927f4a0d46c
coolasu <asunagan@gmail.com>
2026-06-02 23:55:46 -0700

    Add sign-in, sign-up pages and login protection

---
+++ proxy.ts
@@ -0,0 +1,15 @@
+import { auth } from '@/app/lib/auth/server';
+
+export default auth.middleware({ loginUrl: '/auth/sign-in' });
+
+export const config = {
+  matcher: [
+    '/reader/:path*',
+    '/dashboard/:path*',
+    '/dashboard-test/:path*',
+    '/library/:path*',
+    '/bookmarks/:path*',
+    '/profile/:path*',
+    '/settings/:path*',
+  ],
+};
