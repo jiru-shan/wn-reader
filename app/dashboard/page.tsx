5d621b6d866a2f2d7dfc82edadc6a01c05e145b7
Andrew Li <207803156+andrewrli@users.noreply.github.com>
2026-06-03 20:18:39 -0700

    Improve styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,7 +12,7 @@ export default async function DashboardPage() {
 
   return ( 
     <div className="flex flex-col flex-1 items-center justify-center bg-white font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col py-32 px-16 bg-white dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col py-16 px-16 bg-white dark:bg-black">
         <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
           Your collection
         </h1>
