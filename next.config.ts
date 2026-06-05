1cd9047233f5843b4fa8a7b38c62f3cb9875907e
Andrew Li <207803156+andrewrli@users.noreply.github.com>
2026-06-05 05:34:02 -0700

    Redirect / to /dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        // note: if you are not logged in, you further get redirected from /dashboard to /auth/sign-in (see proxy.ts)
+        destination: '/dashboard',
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
