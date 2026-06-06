f78bc06d95b33bf5ddc16da3fe3a6adb226f4846
Andrew Li <207803156+andrewrli@users.noreply.github.com>
2026-06-05 21:40:13 -0700

    Add minimal example test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('redirects if not authenticated', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveURL('http://localhost:3000/auth/sign-in');
 });
 
+/*
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -16,3 +16,4 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+*/
